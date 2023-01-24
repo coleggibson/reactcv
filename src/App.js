dbaf83f4723ccbb7ffd8036a2bd881ea8a6610b7
Cole Gibson <coleggibson@gmail.com>
2023-01-23 20:24:20 -0600

    Created main skeleton, working on General information section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      Name: {
+        text:'',
+        content:''
+      },
+      Email: {
+        text:'',
+        content:''
+    },
+      PhoneNumber: {
+        text:'',
+        content:''
+    },
+  }
 }
+
+  handleChange = (e) => {
+    const value = e.target.value
+
+    this.setState({
+      [e.target.name]: {
+        content: value
+      }
+    })
+  }
+
+  genInfoSubmit = (e) => {
+    // const value = e.target.value
+  e.preventDefault();
+  this.setState ({
+    // ...state,
+    Name: {text:''},
+    Email: {text:''},
+    PhoneNumber: {text:''}
+  });
+  }
+
+  render(){
+      const {Name, Email, PhoneNumber} = this.state;
+      return (
+      <div>
+        <div id="General">General Information
+          <form onSubmit={this.genInfoSubmit}>
+            <label htmlFor="taskInput">Name:</label><br/>
+            <input type="text" name="Name" value={Name.text} onChange={this.handleChange}></input><br/>
+            <label htmlFor="taskInput">Email:</label><br/>
+            <input type="text" name="Email" value={Email.text} onChange={this.handleChange}></input><br/>
+            <label htmlFor="taskInput">Phone Number:</label><br/>
+            <input type="text" name="PhoneNumber" value={PhoneNumber.text} onChange={this.handleChange}></input><br/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div id="Education">Educational Experience
+          <form>
+            <label htmlFor="taskInput">School Name:</label><br/>
+            <input type="text" id="SchoolName" ></input><br/>
+            <label htmlFor="taskInput">Title of Study:</label><br/>
+            <input type="text" id="TitleOfStudy" ></input><br/>
+            <label htmlFor="taskInput">Date of Study:</label><br/>
+            <input type="text" id="DateofStudy" ></input><br/>
+          </form>
+        </div>
+        <div id="Experience">Practical Experience
+          <form>
+            <label htmlFor="taskInput">Company Name:</label><br/>
+            <input type="text" id="CompanyName" ></input><br/>
+            <label htmlFor="taskInput">Position Title:</label><br/>
+            <input type="text" id="PositionTitle" ></input><br/>
+            <label htmlFor="taskInput">Main Tasks of Your Jobs:</label><br/>
+            <input type="text" id="JobTasks" ></input><br/>
+          </form>
+        </div>
+        <General value = {value}/>
+        <div id="educationContent"></div>
+        <div id="experienceContent"></div>
+      </div>
+      );
+    }
+  }
 
 export default App;
---
+++ src/components/General.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const General = (props) => {
+    return (
+    <div>
+        <div>{Name.content}</div>
+        <div>{Email.content}</div>
+        <div>{PhoneNumber.content}</div>
+    </div>
+    );
+};
+
+export default General
